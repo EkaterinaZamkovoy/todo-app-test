5aaff377f98685ba22b3bffab7a6c5ca0f7bbe66
EkaterinaZamkovoy <ekaterinazamkovoy@yandex.ru>
2025-06-23 19:37:39 +0300

    feat: create main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image';
+import { MainToDoWidget } from '@/widget';
 
 export default function Home() {
+  const classNames = {
+    container: 'flex flex-col gap-[50px] px-[100px] py-[100px]',
+    title:
+      'text-6xl font-bold text-primary text-center border border-greyLight p-5 rounded-[20px] shadow-[0_0_10px_rgba(0,0,0,0.1)]',
+  };
+
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='p-8'>
-        <h1 className='text-4xl font-bold text-blue-600'>
-          Привет, Tailwind работает!
-        </h1>
-      </main>
-    </div>
+    <main className={classNames.container}>
+      <div className={classNames.title}>
+        <h1>TODO LIST</h1>
+      </div>
+      <MainToDoWidget />
+    </main>
   );
 }
